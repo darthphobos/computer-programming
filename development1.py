6610003749fccd5dde130c593fab3fb3440a92aa
Scholar <scholar@paradigm.local>
2023-08-31 14:21:03 -0600

    test push

---
+++ development1.py
@@ -0,0 +1,29 @@
+person = "Barrett"
+
+for i in range(10):
+    print(i)
+
+list = ['one', 'two', 'three']
+
+dictionary = {
+    'name': 'Barrett',
+    'age': 16
+}
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print('This is and even number!')
+    else:
+        print('This is an odd number!')
+
+is_even_number(43)
+
+MAIN_CHARACTER = "Steve"
+
+main_character = "Darth Alex"
+
+myNameIsNot = "Mr Ohio"
+
+my_name = "Billy the Kid"
+
+MyName = "Midas touch"
