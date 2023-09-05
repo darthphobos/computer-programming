830baeb15788b03839818e51e18fcca2f9eeadc0
Scholar <scholar@paradigm.local>
2023-09-05 13:27:57 -0600

    variables and a function

---
+++ development2.py
@@ -0,0 +1,17 @@
+my_name = "Ethan" 
+
+test_score = 100 
+
+was_lunch_tasty = True
+
+b_day_classes = ['seminar', 'computer programming', 'kedge', 'a capella']
+
+print(f"Hello my name is{my_name}. The best test score I have ever had is {test_score}. Classes for me today are {b_day_classes}.")
+
+def add_two_numbers(num1 , num2):
+    print(num1 + num2)
+
+user_number1 = int(input("Give me a number:"))
+user_number2 = int(input("Give me a 2nd number:"))
+
+add_two_numbers(user_number1, user_number2)
