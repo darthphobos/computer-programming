17c7b9d35f8c4f5a894b5ffb999652a1109be646
Scholar <scholar@paradigm.local>
2023-09-08 10:31:17 -0600

    concepcts

---
+++ concepets.py
@@ -0,0 +1,44 @@
+your_name = "Brian"
+
+def give_a_name():
+    a_name = "William"
+    print(a_name)
+
+print(your_name)
+
+user_input = input("What is your name? ")
+print(f"Nice to meet you, {user_input}!")
+
+# Data types
+my_string = "This is my string."
+my_integer = 98567
+my_float = 3.14
+is_my_boolean = True 
+
+print(my_integer + my_float)
+
+my_declaration = ""
+my_initialization = "Hello world"
+
+add = 1 + 2
+subtract = 2 - 1
+divide = 2 / 1
+multiply = 1 * 3
+modulo = 10 % 3
+exponent = 2 ** 3
+
+print(modulo)
+
+statement = 4 > 2
+statement2 = 4 >= 2
+statement3 = 4 <= 2
+statement4 = 4 == 2
+statement5 = 4 != 2
+
+print(statement)
+
+message = 4 > 2 and 33 < 43
+message2 = 4 < 2 or 33 < 43
+x = False
+
+print(not x)
