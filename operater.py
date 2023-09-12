0edeb72527591054d74c40b882d815b4dcd6a95a
Scholar <scholar@paradigm.local>
2023-09-12 13:28:38 -0600

    operater

---
+++ operater.py
@@ -0,0 +1,62 @@
+def my_function():
+    print("Hello")
+
+my_function()
+
+def add(num1, num2):
+    print(num1 + num2)
+
+def subtract(num1, num2):
+    return num1 - num2
+
+print(subtract(7,8))
+
+difference = subtract(7,8)
+
+print(subtract)
+
+add(2, 3)
+
+subtract2 = 4 - 2
+print(subtract2)
+
+multiply = 8 * 9
+print(multiply)
+
+divide = 9 / 3
+print(divide)
+
+modulo = 55 % 7 
+print(modulo)
+
+exponent = 4 ** 45
+print(exponent)
+
+statement = 8 > 4
+print(statement)
+
+statement2 = 8 < 12
+print(statement2)
+
+statement3 = 17 <= 23
+print(statement3)
+
+statement4 = 34 >= 19
+print(statement4)
+
+statement5 = 46 == 89
+print(statement5)
+
+statement6 = 96 != 33
+print(statement6)
+
+statement7 = 1000 // 5
+print(statement7)
+
+message = 4 > 5 or 50 > 36
+print(message)
+
+message2 = 2 < 4 and 8 < 9
+print(message2)
+
+print(not message)
