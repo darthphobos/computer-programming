229c605242d646821f20d03bf0693e20ddee4286
Scholar <scholar@paradigm.local>
2023-09-19 13:30:48 -0600

    control and function

---
+++ control.py
@@ -0,0 +1,27 @@
+car_color = "Blue"
+car_make = "Tesla"
+
+if car_color == "Blue":
+    if car_make == "Tesla":
+        print("My car is a Blue Tesla")
+    else:
+        print("My car is blue")
+elif car_color == "Red":
+    print('My car is a ferrari')
+else:
+    print("I have a bike made of monster cans")
+
+    #for loop
+for i in range(1, 10):
+    print(i)
+
+sports = ['soccoer','basketball', 'hockey', 'football']
+
+for item in sports:
+    print(item)
+
+#while loop
+i = 1
+while i < 6 :
+    print("I am still going")
+    i += 1
---
+++ functions2.py
@@ -0,0 +1,4 @@
+def print_my_name(name):
+    print(name)
+
+print(print_my_name)
