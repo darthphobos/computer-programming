e700031c064b4388b95efd564abdcd7ac1cb267e
Scholar <scholar@paradigm.local>
2023-09-29 11:28:42 -0600

    for loops

---
+++ forloops.py
@@ -0,0 +1,10 @@
+word = "paradigm"
+for i in word:
+    print(i)
+
+list1 = [5,10,15,20]
+list2 = ['tomatos', 'Potatos', 'carrots', 'cucmburs']
+
+for i in list1:
+    for j in list2:
+        print(i, j)
