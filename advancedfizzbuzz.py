84b5a041130f0c0d9f6ad0bb5a245806d45bf47e
Scholar <scholar@paradigm.local>
2023-10-19 13:09:44 -0600

    fizzybuzzybeesoda

---
+++ advancedfizzbuzz.py
@@ -0,0 +1,14 @@
+for hundred in range(1, 101):
+    if hundred % 3 == 0:
+        print("fizz")
+    elif hundred % 5 == 0:
+        print("Buzz")
+    else:
+        print(hundred)
+
+# for hundred in range(1, 101):
+#     if hundred % 5 == 0:
+#         beefmealone = "busybuzzybees"
+#         print("busybuzzybees")
+#     else:
+#         print(hundred)
