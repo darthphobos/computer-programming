ec4aa26872f589db7e4cf176b9a663580e06ca3c
Scholar <scholar@paradigm.local>
2023-11-02 13:30:39 -0600

    interview

--- career.py
+++ career.py
@@ -1,3 +1,21 @@
-name = input("Welcome to the interview what is your name? ")
+userinput1 = input("Welcome to the interview what is your name? ")
 
-print(f"Hello {name}")
+print(f"Hello {userinput1}")
+
+userinput2 = input("Ok what career are you interested in this field? ")
+
+userinput3 = input("What are your years of experince in programming? ")
+
+userinput4 = input("What is your desired salary? ")
+
+userinput5 = input("Tell me about your hobbies? ")
+
+userinput6 = input("Do you have a crimnal record? if so tell me what crimes you have commited? ")
+
+userinput7 = input("What do you want to contribit to this company or what is your vision? ")
+
+userinput8 = input("Do you dislike animals? if so why is that? ")
+
+userinput9 = input("Do you know muiltple coding languages? ")
+
+userinput10 = input("What did you want to be growing up as a child? ")
