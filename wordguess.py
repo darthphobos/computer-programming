89ee23bc240c691015230f873b4d1414609847c4
Scholar <scholar@paradigm.local>
2023-11-16 13:09:31 -0700

    Guess a word

--- wordguess.py
+++ wordguess.py
@@ -1,1 +1,20 @@
-word = int(())
+word = "MARKARAGNOS"
+guesses = "_ _ _ _ _ _ _ _ _ _ _"
+win = False 
+while win == False:
+    loopword = "" 
+    guess = input("Guess a ladder letter")
+    if len(guess) > 1:
+        if guess == word:
+            win = True 
+            break 
+    if word.find(guess) != -1:
+        for i in range(len(word)):
+            if guess == word[i] or guesses.find(word[i]) > -1:
+                loopword = loopword + word[i] + " "
+            else:
+                loopword = loopword + "_ "
+    guesses = loopword
+    print(guesses)
+
+print("Go touch grass for wasting your time on this computer")
