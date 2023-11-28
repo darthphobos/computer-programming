57adc5a1c035ed12cf01eef6e94420f063de7ee7
Scholar <scholar@paradigm.local>
2023-11-28 13:27:15 -0700

    hangman

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 
 import random 
-  
-# Open the file in read mode 
+   
 with open("sowpods.txt", "r") as file: 
     allText = file.read() 
     words = list(map(str, allText.split())) 
@@ -9,23 +8,36 @@ with open("sowpods.txt", "r") as file:
     # print random string 
     print(random.choice(words)) 
     
-word = "MARKARAGNOS"
-guesses = "_ _ _ _ _ _ _ _ _ _ _"
-win = False 
-while win == False:
-    loopword = "" 
-    guess = input("Guess a ladder letter")
-    if len(guess) > 1:
-        if guess == word:
-            win = True 
-            break 
-    if word.find(guess) != -1:
-        for i in range(len(word)):
-            if guess == word[i] or guesses.find(word[i]) > -1:
-                loopword = loopword + word[i] + " "
-            else:
-                loopword = loopword + "_ "
-    guesses = loopword
-    print(guesses)
-
-print("Go touch grass for wasting your time on this computer")
+word = random.choice(words)
+guessed = "_" * len(word)
+word = list(word)
+guessed = list(guessed)
+listGuessed = []
+letter = input("guess letter: ").upper()
+lives = 10 
+while True:
+    if letter in listGuessed:
+        letter = ""
+        print("Already guessed!!")
+    elif letter in word: 
+        index = word.index(letter)
+        guessed[index] = letter
+        word[index] = '_'
+    else:
+        print(''.join(guessed))
+        if letter != '':
+            listGuessed.append(letter)
+        letter = input("guess letter: ").upper()
+    
+    if letter not in guessed:
+        lives -= 1
+        print(f"You have {lives} lives left")
+    
+    if '_' not in guessed:
+        print("YOU WON")
+        print(f"Your word was: {''.join(guessed)}")
+        exit()
+    
+    if lives == 0:
+        print("Your a skill issue")
+        exit()
