782591747cf8c6e7aae721d7a6c7477a8bcd904f
Scholar <scholar@paradigm.local>
2023-11-30 13:34:19 -0700

    Rpg

---
+++ RPG.py
@@ -0,0 +1,6 @@
+Health = 100
+
+Name = input("Hello dinn- I mean hello weary traveler welcome to my tropical island. ")
+print(f"What is your {Name}? Before you go out hunting monsters for me I need all you to sign relase forms for in the case you don't come back and I need all your personal information. ")
+print("Thanks for giving me your signing and giving all your personal information. You need to go hunt the monsters off my bermuda triangle where none return. ")
+user_input_one = print("Press w to be eat- I mean to walk Press i to check to see if you can be cook- I mean to check your inventory and health. ")
