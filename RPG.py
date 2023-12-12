9586a72acd663488fa1ddbcacb5e5b21e5292b97
Scholar <scholar@paradigm.local>
2023-12-12 12:50:34 -0700

    rpg

--- RPG.py
+++ RPG.py
@@ -1,12 +1,13 @@
 import random
 
 Player_Health = 100
-enemy_health = 100
 inventory = []
 
-def attack_enemy(Player_Health, enemy_health):
+def attack_enemy(Player_Health):
+    enemy_health = 100
+    Player_health = Player_Health
     while True:
-        if Player_Health > 0:
+        if Player_health > 0:
             your_attack = random.randint(10, 100)
             print(f"You attacked your enemy for {your_attack} damage!")
             enemy_health -= your_attack
@@ -14,41 +15,18 @@ def attack_enemy(Player_Health, enemy_health):
         if enemy_health > 0:
             enemy_attack = random.randint(10, 100)
             print(f"the enemy attacked you for {enemy_attack} damage!")
-            Player_Health -= enemy_attack 
+            Player_health -= enemy_attack 
         
-        if Player_Health <= 0:
+        if Player_health <= 0:
             print("You are tasty you are not getting away dinner!!")
             exit()
 
         elif enemy_health <= 0:
             print("I now have the urge to eat him.")
             print("You recived 25 health and an item in your invetory from eating your attacker")
-            Player_Health += 25
+            Player_health += 25
             inventory.append("sword")
-            break
-
-def enemy_attacks_you(Player_Health, enemy_health):
-    while True:
-        if enemy_health > 0:
-            enemy_attack = random.randint(10, 100)
-            print(f"the enemy attacked you for {enemy_attack} damage!")
-            Player_Health -= enemy_attack 
-        
-        if Player_Health > 0:
-            your_attack = random.randint(10, 100)
-            print(f"You attacked your enemy for {your_attack} damage!")
-            enemy_health -= your_attack
-        
-        if Player_Health <= 0:
-            print("You are tasty you are not getting away dinner!!")
-            exit()
-
-        elif enemy_health <= 0:
-            print("I now have the urge to eat him.")
-            print("You recived 25 health and an item in your invetory from eating your attacker")
-            Player_Health += 25
-            inventory.append("sword")
-            break
+            return(Player_health)
 
 
 Name = input("Hello dinn- I mean hello weary traveler welcome to my tropical island. what is your name?  ")
@@ -69,12 +47,14 @@ while True:
                 print("You ran into a uncontacted cannible tribe")
             attack_choice = input("Would you like to attack or run: a or r")
             if attack_choice == "a":
-                attack_enemy(Player_Health, enemy_chance)
+                Player_Health = attack_enemy(Player_Health)
             elif attack_choice == "r":
                 run_chance = random.randint(1,2)
                 if run_chance == 1:
-                    enemy_attacks_you(Player_Health, enemy_health)
+                    Player_Health = attack_enemy(Player_Health)
                 else:
                     print("You got away!")
+                    print(f"You have {Player_Health} health left and you have {inventory} in your inventory")
+                    break
     elif user_input_one == "i":
         print(f"You have {Player_Health} health left and you have {inventory} in your inventory")
